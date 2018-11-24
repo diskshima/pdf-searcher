a01c9c8935af7ceac98ae3f4555ded8818543bbb
Daisuke Shimamoto <diskshima@gmail.com>
2018-11-24 16:10:29 +0900

    Show item details

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,17 @@ const PDFJS: PDFJSStatic = require( "pdfjs-dist" );
 async function readDocument(path: string): Promise<void> {
   let doc = await PDFJS.getDocument(filepath);
   let outline = await doc.getOutline();
-  console.log(outline.map(e => e.title));
+  let pages = doc.numPages;
+  let titles = outline.map(e => e.title);
+
+  for (let i = 1; i <= 1; i++) {
+    let page = await doc.getPage(i);
+    let textContent = await page.getTextContent();
+
+    console.log(`----${i}----`);
+    let items = textContent.items;
+    console.log(items);
+  }
 }
 
 let filepath = process.argv[2];
