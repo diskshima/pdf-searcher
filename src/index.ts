67c292b665c352a07676df803c36f2b9b15be4a6
Daisuke Shimamoto <diskshima@gmail.com>
2018-11-25 02:02:38 +0900

    [WIP] Add PDF page content to ES

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import '@babel/polyfill';
 import { PDFDocumentProxy, PDFJSStatic } from 'pdfjs-dist';
-import { Client } from 'elasticsearch';
+import { Client, GetResponse } from 'elasticsearch';
 
 const PDFJS: PDFJSStatic = require( "pdfjs-dist" );
+
+const DOC_TYPE = '_doc';
 
 async function readDocument(path: string): Promise<void> {
   let doc = await PDFJS.getDocument(filepath);
@@ -10,32 +12,62 @@ async function readDocument(path: string): Promise<void> {
   let pages = doc.numPages;
   let titles = outline.map(e => e.title);
 
-  for (let i = 1; i <= 1; i++) {
+  for (let i = 1; i <= 10; i++) {
     let page = await doc.getPage(i);
     let textContent = await page.getTextContent();
 
-    console.log(`----${i}----`);
+    console.log(`Processing ${i}...`);
     let items = textContent.items;
-    console.log(items);
+    insertPage(titles[0], i, items.map(e => e.str).join(''));
   }
-}
+};
 
-let queryES = async () => {
-  let client = new Client({
+let getClient = () => {
+  return new Client({
     host: 'localhost:9200',
     log: 'trace',
   });
+};
 
-  let error = await client.ping({});
-  if (error) {
-    console.log('Error:', error);
-  } else {
-    console.log('Ping worked.');
+let insertPage = async (title: string, pageNumber: number, text: string) => {
+  let client = getClient();
+  let document = {
+    document: {
+      title,
+    },
+    page: {
+      number: pageNumber,
+    },
+    content: {
+      text,
+    }
+  };
+
+  try {
+    await client.create({
+      index: 'documents',
+      type: DOC_TYPE,
+      id: `${title}_${pageNumber}`,
+      body: document,
+    });
+
+    return;
+  } catch (e) {
+    console.warn(e);
   }
+};
+
+let getPage = async (index: string , id: string) => {
+  let client = getClient();
+
+  let resp = await client.get({ id, index, type: DOC_TYPE });
+  return resp;
 };
 
 let filepath = process.argv[2];
 
 // readDocument(filepath);
 
-queryES();
+getPage('documents', 'クラウドサービス事業者が医療情報を取り扱う際の安全管理に関するガイドライン第1版_9').then((page) => {
+  console.log(page._source);
+});
